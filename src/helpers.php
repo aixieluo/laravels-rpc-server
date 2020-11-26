cbc46b3952f3f7084d83c43182bfde9fe361f760
xuxiaoyang <admin@aixieluo.com>
2020-11-26 13:44:27 +0800

    init

---
+++ src/helpers.php
@@ -0,0 +1,47 @@
+<?php
+
+if (! function_exists('app')) {
+    /**
+     * Get the available container instance.
+     *
+     * @param string|null $abstract
+     * @param array       $parameters
+     *
+     * @return mixed|\Illuminate\Contracts\Foundation\Application
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    function app($abstract = null, array $parameters = [])
+    {
+        if (is_null($abstract)) {
+            return \Illuminate\Container\Container::getInstance();
+        }
+
+        return \Illuminate\Container\Container::getInstance()->make($abstract, $parameters);
+    }
+}
+
+if (! function_exists('request')) {
+    /**
+     * Get an instance of the current request or an input item from the request.
+     *
+     * @param array|string|null $key
+     * @param mixed             $default
+     *
+     * @return \Illuminate\Http\Request|string|array
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     */
+    function request($key = null, $default = null)
+    {
+        if (is_null($key)) {
+            return app('request');
+        }
+
+        if (is_array($key)) {
+            return app('request')->only($key);
+        }
+
+        $value = app('request')->__get($key);
+
+        return is_null($value) ? value($default) : $value;
+    }
+}
