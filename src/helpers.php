f8dd3bdbc894439399cc738faa0f70e6e1c1f6b8
xuxiaoyang <admin@aixieluo.com>
2020-11-30 15:05:16 +0800

    rm app

--- src/helpers.php
+++ src/helpers.php
@@ -1,25 +1,5 @@
 <?php
 
-if (! function_exists('app')) {
-    /**
-     * Get the available container instance.
-     *
-     * @param string|null $abstract
-     * @param array       $parameters
-     *
-     * @return mixed|\Illuminate\Contracts\Foundation\Application
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
-     */
-    function app($abstract = null, array $parameters = [])
-    {
-        if (is_null($abstract)) {
-            return \Illuminate\Container\Container::getInstance();
-        }
-
-        return \Illuminate\Container\Container::getInstance()->make($abstract, $parameters);
-    }
-}
-
 if (! function_exists('req')) {
     /**
      * Get an instance of the current request or an input item from the request.
@@ -29,9 +9,9 @@ if (! function_exists('req')) {
      */
     function req()
     {
-        app()->singletonIf('request', function () {
+        \Illuminate\Container\Container::getInstance()->singletonIf('request', function () {
             return \Illuminate\Http\Request::capture();
         });
-        return  app('request');
+        return  \Illuminate\Container\Container::getInstance()->make('request');
     }
 }
